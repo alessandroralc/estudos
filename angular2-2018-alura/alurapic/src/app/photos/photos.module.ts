db026bbedc07173b86daca20a5fc7552fb3be3c0
Alessandro Reis de Alcântara <ralessan@trt3.jus.br>
2018-08-17 08:38:38 -0300

    Continuando alurapic.

--- angular2-2018-alura/alurapic/src/app/photos/photos.module.ts
+++ angular2-2018-alura/alurapic/src/app/photos/photos.module.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+
+import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoComponent } from './photo/photo.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 @NgModule({
     declarations: [
-        PhotoComponent
+        PhotoComponent,
+        PhotoListComponent,
+        PhotoFormComponent,
+        PhotosComponent
     ],
-    exports: [
-        PhotoComponent
+    imports: [
+        HttpClientModule,
+        CommonModule
     ]
 })
 export class PhotosModule { }
