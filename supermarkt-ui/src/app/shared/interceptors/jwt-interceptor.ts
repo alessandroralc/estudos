40ef20c63c00f3e909aa4276ee55d26bdd691104
Alessandro Reis de Alcântara <alessandroralc@gmail.com>
2019-11-26 15:22:09 -0300

    [supermarkt] JWT

--- supermarkt-ui/src/app/shared/interceptors/jwt-interceptor.ts
+++ supermarkt-ui/src/app/shared/interceptors/jwt-interceptor.ts
@@ -1,2 +1,20 @@
-export class JwtInterceptor {
+import { HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { AutenticacaoService } from '../services/autenticacao.service';
+
+export class JwtInterceptor implements HttpInterceptor {
+
+    constructor(private autenticacaoService: AutenticacaoService) {}
+    
+    intercept(request: HttpRequest<any>, next: import("@angular/common/http").HttpHandler): import("rxjs").Observable<import("@angular/common/http").HttpEvent<any>> {
+        const currentUser = this.autenticacaoService.currentUserValue;
+        if (currentUser && currentUser.token) {
+            request = request.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${currentUser.token}`
+                }
+            })
+        }
+
+        return next.handle(request);
+    }
 }
