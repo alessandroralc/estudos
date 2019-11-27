878a478a719368dddb40b3918dd43c38fe90992b
Alessandro Reis de Alcântara <alessandroralc@gmail.com>
2019-11-27 11:26:19 -0300

    [supermarkt] Erros module pronto

--- supermarkt-ui/src/app/shared/shared.module.ts
+++ supermarkt-ui/src/app/shared/shared.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DirectivesModule } from './directives/directives.module';
+import { PipesModule } from './pipes/pipes.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptor } from './interceptors/jwt-interceptor';
+import { ErroServidorInterceptor } from './interceptors/erro-servidor-interceptor';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    PipesModule,
+    DirectivesModule
+  ],
+  exports: [
+    PipesModule,
+    DirectivesModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErroServidorInterceptor, multi: true }
   ]
 })
 export class SharedModule { }
